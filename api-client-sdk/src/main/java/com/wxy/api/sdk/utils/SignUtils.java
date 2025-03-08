031bb9c90e1665c9f192c867e3be038a8c140630
巫鑫阳 <3240028935@qq.com>
2025-03-08 14:13:53 +0800

    sdk

---
+++ api-client-sdk/src/main/java/com/wxy/api/sdk/utils/SignUtils.java
@@ -0,0 +1,23 @@
+package com.wxy.api.sdk.utils;
+
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.Digester;
+
+/**
+ * 签名工具
+ */
+public class SignUtils {
+
+    /**
+     * 生成签名
+     * @param body
+     * @param secretKey
+     * @return
+     */
+    public static String getSign(String body, String secretKey) {
+        Digester md5 = new Digester(DigestAlgorithm.SHA256); // 选择加密算法
+        String content = body + "." + secretKey;
+        String digestHex = md5.digestHex(content);
+        return digestHex;
+    }
+}
